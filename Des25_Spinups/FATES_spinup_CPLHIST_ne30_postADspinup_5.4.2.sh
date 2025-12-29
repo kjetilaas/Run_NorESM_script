a79b75f0a66751dca6173c4ea1b1e2bbe7815b13
kjetilaas <kjetil.aas@cicero.oslo.no>
2025-12-29 16:27:47 +0100

    Land spinup scripts for NorESM3b09

---
+++ Des25_Spinups/FATES_spinup_CPLHIST_ne30_postADspinup_5.4.2.sh
@@ -0,0 +1,163 @@
+#!/bin/bash 
+
+#Scrip to clone, build and run NorESM on Betzy
+
+dosetup1=1 #do first part of setup
+dosetup2=1 #do second part of setup (after first manual modifications)
+dosetup3=1 #do second part of setup (after namelist manual modifications)
+dosubmit=1 #do the submission stage
+forcenewcase=1 #scurb all the old cases and start again
+
+echo "setup1, setup2, setup3, submit, forcenewcase:", $dosetup1, $dosetup2, $dosetup3, $dosubmit, $forcenewcase
+
+USER="kjetisaa"
+project='nn9560k' #nn8057k: EMERALD, nn2806k: METOS, nn9188k: CICERO, nn9560k: NorESM (INES2), nn9039k: NorESM (UiB: Climate predition unit?), nn2345k: NorESM (EU projects)
+machine='betzy'
+
+#NorESM dir
+noresmrepo="ctsm5.4.002_noresm_v1" 
+noresmversion="ctsm5.4.002_noresm_v1"
+
+resolution="ne30pg3_tn14" #f19_g17, ne30pg3_tn14, f45_f45_mg37, ne16pg3_tn14 
+casename="i1850.$resolution.$noresmversion.CPLHIST_postADspinup.`date +"%Y-%m-%d"`"
+echo "casename: $casename"
+compset="1850_DATM%CPLHIST_CLM60%FATES-NOCOMP_SICE_SOCN_SROF_SGLC_SWAV_SESP"
+
+# aka where do you want the code and scripts to live?
+workpath="/cluster/work/users/$USER/" 
+
+# some more derived path names to simplify scripts
+scriptsdir=$workpath$noresmrepo/cime/scripts/
+
+#case dir
+casedir=$workpath$casename
+
+#where are we now?
+startdr=$(pwd)
+
+#Download code and checkout externals
+if [ $dosetup1 -eq 1 ] 
+then
+    cd $workpath
+
+    pwd
+    #go to repo, or checkout code
+    if [[ -d "$noresmrepo" ]] 
+    then
+        cd $noresmrepo
+        echo "Already have NorESM repo"
+    else
+        echo "Cloning NorESM"
+        
+        if [[ $noresmversion == ctsm* ]] ; then
+            echo "Using CTSM version $noresmversion"
+            git clone https://github.com/NorESMhub/CTSM/ $noresmrepo
+        else
+            echo "Using NorESM version $noresmversion"
+            git clone https://github.com/NorESMhub/NorESM/ $noresmrepo
+        fi
+        cd $noresmrepo
+        git checkout $noresmversion
+        ./bin/git-fleximod update
+        echo "Built model here: $workpath$noresmrepo"        
+
+    fi
+fi
+
+#Make case
+if [[ $dosetup2 -eq 1 ]] 
+then
+    cd $scriptsdir
+
+    if [[ $forcenewcase -eq 1 ]]
+    then 
+        if [[ -d "$workpath$casename" ]] 
+        then    
+        echo "$workpath$casename exists on your filesystem. Removing it!"
+        rm -rf $workpath$casename
+        rm -r $workpath/noresm/$casename
+        rm -r $workpath/archive/$casename
+        rm -r $casename
+        fi
+    fi
+    if [[ -d "$workpath$casename" ]] 
+    then    
+        echo "$workpath$casename exists on your filesystem."
+    else
+        
+        echo "making case:" $workpath$casename        
+        ./create_newcase --case $workpath$casename --compset $compset --res $resolution --project $project --run-unsupported --mach betzy --pecount L
+
+        cd $workpath$casename
+        export LMOD_IGNORE_CACHE=1
+        #XML changes
+        echo 'updating settings'         
+        ./xmlchange DATM_CPLHIST_CASE=n1850.ne30_tn14.nor3_b01-cplhist-noLU.20250716
+        ./xmlchange DATM_CPLHIST_DIR=/cluster/shared/noresm/inputdata/cplhist/noresm3_0/n1850.ne30_tn14.nor3_b01-cplhist-noLU.20250716/
+        ./xmlchange DATM_PRESNDEP=none
+        ./xmlchange DATM_YR_START=51
+        ./xmlchange DATM_YR_END=100
+        ./xmlchange CLM_ACCELERATED_SPINUP=off
+        ./xmlchange DATM_PRESAERO=clim_1850 
+        ./xmlchange RUN_STARTDATE=0151-01-01
+        ./xmlchange STOP_OPTION=nyears
+        ./xmlchange STOP_N=50
+        ./xmlchange RESUBMIT=6
+        ./xmlchange --subgroup case.run JOB_WALLCLOCK_TIME=36:00:00
+        ./xmlchange --subgroup case.st_archive JOB_WALLCLOCK_TIME=00:30:00        
+        
+        echo 'done with xmlchanges'        
+        
+        ./case.setup
+        echo ' '
+        echo "Done with Setup. Update namelists in $workpath$casename/user_nl_*"
+
+        #Add following lines to user_nl_clm   
+cat <<EOF >> user_nl_clm
+glacier_region_behavior = 'single_at_atm_topo','UNSET','virtual','virtual' 
+glcmec_downscale_longwave = .false. 
+precip_repartition_glc_all_rain_t = 2. 
+precip_repartition_glc_all_snow_t = 0. 
+snow_thermal_cond_glc_method = 'Jordan1991'
+albice = 0.6,0.4
+use_fates_nocomp=.true.
+use_fates_fixed_biogeog=.true.
+fates_stomatal_model='medlyn2011'
+fates_spitfire_mode=4
+use_fates_luh=.true.
+use_fates_lupft=.true.
+fates_harvest_mode='luhdata_area'
+use_fates_potentialveg=.false.
+do_transient_lakes = .false.
+do_transient_urban = .false.
+hist_empty_htapes = .true.
+hist_fincl1 = 'FATES_NOCOMP_PATCHAREA_PF', "TOTEXICE_VOL", "TOTSOILICE", "TOTSOILLIQ",'ALT','ALTMAX','TSL','FATES_FRACTION','TSA','RAIN','SNOW','EFLX_LH_TOT','FSH','QSOIL','TLAI','FCO2','TOTSOMC','TOTSOMC_1m','FATES_VEGC','FATES_GPP','FATES_NEP','FATES_NPP','TWS','H2OSNO','FSNO','TOT_WOODPRODC','PROD100C','PROD10C','FATES_LITTER_AG_CWD_EL','FATES_LITTER_AG_FINE_EL','FATES_LITTER_BG_CWD_EL','FATES_LITTER_BG_FINE_EL','FATES_GRAZING','FATES_FIRE_CLOSS','FATES_BURNFRAC'
+hist_mfilt=1
+hist_nhtfrq=0  
+fates_paramfile = '/cluster/shared/noresm/inputdata/lnd/clm2/paramdata/fates_params_sci.1.88.6_api.42.0.0_14pft_nor_sci3_api1_c251218.nc'
+finidat = '/cluster/work/users/kjetisaa/archive/i1850.ne30pg3_tn14.ctsm5.4.002_noresm_v1.CPLHIST_ADspinup_pt2.2025-12-19/rest/0151-01-01-00000/i1850.ne30pg3_tn14.ctsm5.4.002_noresm_v1.CPLHIST_ADspinup_pt2.2025-12-19.clm2.r.0151-01-01-00000.nc'
+fluh_timeseries='/cluster/shared/noresm/inputdata/LU_data_CMIP7/LUH3_states_transitions_management.timeseries_ne30_hist_steadystate_1850_2025-06-03_cdf5.nc'
+flandusepftdat='/cluster/shared/noresm/inputdata/LU_data_CMIP7/fates_landuse_pft_map_to_surfdata_ne30np4_250530_cdf5.nc'
+EOF
+
+    fi
+fi
+
+#Build case case
+if [[ $dosetup3 -eq 1 ]] 
+then
+    cd $workpath$casename
+    echo "Currently in" $(pwd)
+    ./case.build
+    echo ' '    
+    echo "Done with Build"
+fi
+
+#Submit job
+if [[ $dosubmit -eq 1 ]] 
+then
+    cd $workpath$casename
+    ./case.submit
+    echo " "
+    echo 'done submitting'       
+fi
